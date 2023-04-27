b1733b3221a7296220fa45d5cd48f3f2a7097c2c
Алексей <setgecko@gmail.com>
2023-04-27 22:02:38 +0300

    Добавьте файлы проекта.

---
+++ Task_13.1.3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_13._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray =
+          {
+               { 5, 8, 3, 10 },
+               { 13, 2, 1, 7 },
+               { 0, 2, 5, 2 },
+               { 3, 8, 3, 45 },
+               { 2, 4, 31, 4 }
+           };
+
+            // Двумерный массив можно представить в виде таблицы.
+
+            //  сохраним длину первого измерения массива (ширина таблицы)
+            int rowLength = myArray.GetLength(0);
+
+            //  сохраним длину второго измерения массива (высота таблицы)
+            int columnLength = myArray.GetLength(1);
+
+            // проход по рядам
+            for (int i = 0; i < rowLength; i++)
+            {
+                // проход по колонкам
+                for (int j = 0; j < columnLength; j++)
+                    Console.WriteLine(myArray[i, j]);
+            }
+        }
+    }
+}
