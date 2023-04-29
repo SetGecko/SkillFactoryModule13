4e27ebefa021f8a5d12f1270d4d64c1ddf24f14d
Алексей <setgecko@gmail.com>
2023-04-29 11:20:59 +0300

    Выполнил задание 13.1.6

---
+++ Task_13.1.6/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_13._1._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // читаем весь файл с рабочего стола в строку текста
+            string text = File.ReadAllText("cdev_Text.txt");
+
+            // Сохраняем символы-разделители в массив
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+            // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            // выводим количество
+            Console.WriteLine(words.Length);
+        }
+    }
+}
