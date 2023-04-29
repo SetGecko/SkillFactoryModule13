b733ad852c88cb6a8339e7909c75f7e42e08cd4a
Алексей <setgecko@gmail.com>
2023-04-29 12:58:06 +0300

    Выполнил задание 13.2.6

---
+++ Task_13.2.6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace Task_13._2._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayList = new ArrayList()
+           {
+               1,
+               "Няка ",
+               "Пука ",
+               777,
+           };
+
+            int sum = 0;
+
+            StringBuilder text = new StringBuilder();
+
+            foreach (var element in arrayList)
+            {
+                if (element is int)
+                {
+                    sum += (int)element;
+                }
+
+                if (element is string s)
+                {
+                    text.Append(element);
+                }
+            }
+
+            var result = new ArrayList() { sum, text.ToString() };
+
+            foreach (var elem in result)
+            {
+                Console.WriteLine(elem);
+            }
+        }
+    }
+}
