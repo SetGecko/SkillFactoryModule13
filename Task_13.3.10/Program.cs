7e6871afed6d9450f1b8047e42da2497b433ae72
Алексей <setgecko@gmail.com>
2023-05-01 13:46:33 +0300

    Выполнил задание 13.3.11

---
+++ Task_13.3.10/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_13._3._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence =
+                "Подсчитайте, сколько уникальных символов в этом предложении, " +
+                "используя HashSet<T>, учитывая знаки препинания, но не учитывая " +
+                "пробелы в начале и в конце предложения.";
+
+            var characters = sentence.ToCharArray();
+            var symbols = new HashSet<char>();
+
+            foreach (var symbol in characters)
+                symbols.Add(symbol);
+
+            Console.WriteLine(symbols.Count);
+        }
+    }
+}
---
+++ Task_13.3.11/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_13._3._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence =
+                    "Подсчитайте, сколько уникальных символов в этом предложении, " +
+                    "используя HashSet<T>, учитывая знаки препинания, но не учитывая " +
+                    "пробелы в начале и в конце предложения.";
+
+            var characters = sentence.ToCharArray();
+            var symbols = new HashSet<char>();
+
+            foreach (var symbol in characters)
+                symbols.Add(symbol);
+
+            var signs = new[] { ',', ' ', '.' };
+
+            var numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            bool containsNumbers = symbols.Overlaps(numbers);
+            Console.WriteLine($"Коллекция содержит цифры: {containsNumbers}");
+
+            symbols.ExceptWith(signs);
+            Console.WriteLine($"Символов без знаков препинания:: {symbols.Count}");
+
+            Console.WriteLine(symbols.Count);
+        }
+    }
+}
