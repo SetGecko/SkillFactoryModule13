b227ebfeebf8a5771b68096fc81916d5566f2c2c
Алексей <setgecko@gmail.com>
2023-05-01 16:31:04 +0300

    Практика 13.5

---
+++ Practice_13.5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Practice_13._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создадим числовой стек
+            Stack<int> numbers = new Stack<int>();
+
+            numbers.Push(3); // в стеке 3
+            numbers.Push(5); // в стеке 5, 3
+            numbers.Push(8); // в стеке 8, 5, 3
+
+            Console.WriteLine("Элементы числового стека: ");
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+            Console.WriteLine($"Извлекаем верхний элемент из стека: {numbers.Pop()}");
+            //  в стеке остались числа 5 и 3
+
+            Console.WriteLine();
+
+            // создадим стек объектов
+            Stack<Person> persons = new Stack<Person>();
+            persons.Push(new Person() { Name = "Иван" });
+            persons.Push(new Person() { Name = "Сергей" });
+            persons.Push(new Person() { Name = "Анна" });
+
+            Console.WriteLine("Элементы стека объектов: ");
+            foreach (Person p in persons)
+                Console.WriteLine(p.Name);
+
+            Console.WriteLine($"Извлекаем верхний элемент из стека объектов: {persons.Pop().Name}");
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Task_13.4.5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace Task_13._4._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запустим таймер
+            var watchTwo = Stopwatch.StartNew();
+
+            // Выполним вставку
+            PhoneBook.TryAdd("Диана", new Contact(79160000002, "diana@example.com"));
+
+            // Выведем результат
+            Console.WriteLine($"Вставка в  словарь: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+        }
+        private static Dictionary<string, Contact> PhoneBook = new Dictionary<String, Contact>()
+        {
+            ["Игорь"] = new Contact(79990000000, "igor@example.com"),
+            ["Андрей"] = new Contact(79990000001, "andrew@example.com"),
+        };
+    }
+}
