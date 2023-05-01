45bd0c35838b53eed75fcc1fe0b66fcf7bb83765
Алексей <setgecko@gmail.com>
2023-05-01 16:09:24 +0300

    Задание 13.4.4

---
+++ Task_13.4.4/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Task_13._4._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Покажем весь список
+            Console.WriteLine("Текущий список контактов: ");
+            WriteAllContacts();
+
+            // Попробуем добавить новый контакт, если такого ещё нет
+            PhoneBook.TryAdd("Диана", new Contact(79160000002, "diana@example.com"));
+
+            //  Выведем обновлённый список
+            Console.WriteLine("Обновленный список контактов: ");
+            WriteAllContacts();
+
+            //  Попробуем достать контакт для изменения данных
+            if (PhoneBook.TryGetValue("Диана", out Contact contact))
+                contact.PhoneNumber = 79990000001;
+
+            // И покажем результат после изменения
+            Console.WriteLine("Список после изменения: ");
+            WriteAllContacts();
+        }
+        public static void WriteAllContacts()
+        {
+            foreach (var contact in PhoneBook)
+                Console.WriteLine(contact.Key + ": " + contact.Value.PhoneNumber);
+            Console.WriteLine();
+        }
+        private static Dictionary<string, Contact> PhoneBook = new Dictionary<String, Contact>()
+        {
+            ["Игорь"] = new Contact(79990000000, "igor@example.com"),
+            ["Андрей"] = new Contact(79990000001, "andrew@example.com"),
+        };
+    }
+    public class Contact // модель класса
+    {
+        public Contact(long phoneNumber, String email) // метод-конструктор
+        {
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+        public long PhoneNumber { get; set; }
+        public String Email { get; set; }
+    }
+}
