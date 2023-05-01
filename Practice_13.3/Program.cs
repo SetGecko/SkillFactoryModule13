d28073615d675f961f48683f1d7b04ced6a496a4
Алексей <setgecko@gmail.com>
2023-05-01 13:13:37 +0300

    Практика 13.3

---
+++ Practice_13.3/Program.cs
@@ -0,0 +1,33 @@
+﻿// using System.Collections.Generic;
+
+namespace Practice_13._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneBook = new List<Contact>();
+
+            phoneBook.Add(new Contact("Жужа", 89000000000, "zhuzha@gmail.com"));
+            phoneBook.Add(new Contact("Пуци", 89111111111, "putsi@gmail.com"));
+
+            foreach (var contact in phoneBook) 
+            { 
+                Console.WriteLine(contact.Name + ": " + contact.PhoneNumber + " email: " + contact.Email);
+            }
+        }
+    }
+    public class Contact
+    {
+        public Contact(string name, long phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+        public string Name { get; }
+        public string Email { get; }
+        public long PhoneNumber { get; }
+
+    }
+}
