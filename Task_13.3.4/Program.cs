9619236874e0dc95f4b9090a95da512201ccd967
Алексей <setgecko@gmail.com>
2023-05-01 13:19:36 +0300

    Выполнил задание 13.3.4

---
+++ Task_13.3.4/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Task_13._3._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        private static void AddUnique(Contact newContact, List<Contact> phoneBook)
+        {
+            bool alreadyExists = false;
+
+            foreach (var contact in phoneBook)
+            {
+                if (contact.Name == newContact.Name)
+                {
+                    alreadyExists = true;
+                    break;
+                }
+            }
+
+            if (!alreadyExists)
+                phoneBook.Add(newContact);
+
+            phoneBook.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
+
+            foreach (var contact in phoneBook)
+                Console.WriteLine(contact.Name + ": " + contact.PhoneNumber);
+        }
+    }
+    public class Contact
+    {
+        public Contact(string name, long phoneNumber, String email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
